aa1ee9c2987d9a1b214960daab9f0587c1d0482d
Steve Jones <stevemrjones@gmail.com>
2026-01-21 14:16:39 -0500

    Updating comments and auto formatings

--- include/braincloud/BrainCloudCustomEntity.h
+++ include/braincloud/BrainCloudCustomEntity.h
@@ -6,12 +6,12 @@
 #pragma clang diagnostic ignored "-Wdocumentation"
 #endif
 
-
 #include <string>
 
 #include "braincloud/BrainCloudTypes.h"
 
-namespace BrainCloud {
+namespace BrainCloud
+{
 
 	class IServerCallback;
 	class BrainCloudClient;
@@ -19,7 +19,7 @@ namespace BrainCloud {
 	class BrainCloudCustomEntity
 	{
 	public:
-		BrainCloudCustomEntity(BrainCloudClient* in_client);
+		BrainCloudCustomEntity(BrainCloudClient *in_client);
 
 		/**
 		 * Creates new custom entity.
@@ -35,7 +35,7 @@ namespace BrainCloud {
 		 * @param isOwned
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void createEntity(const char * in_entityType, const std::string& in_jsonEntityData, const std::string& in_jsonEntityAcl, int64_t in_timeToLive, bool in_isOwned, IServerCallback * in_callback = NULL);
+		void createEntity(const char *in_entityType, const std::string &in_jsonEntityData, const std::string &in_jsonEntityAcl, int64_t in_timeToLive, bool in_isOwned, IServerCallback *in_callback = NULL);
 
 		/**
 		 * Deletes the specified custom entity on the server.
@@ -48,7 +48,7 @@ namespace BrainCloud {
 		 * @param version
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void deleteEntity(const char * in_entityType, const char * in_entityId, const std::string& in_jsonEntityData, int32_t in_version, IServerCallback * in_callback = NULL);
+		void deleteEntity(const char *in_entityType, const char *in_entityId, const std::string &in_jsonEntityData, int32_t in_version, IServerCallback *in_callback = NULL);
 
 		/**
 		 * Deletes the specified custom entity on the server.
@@ -60,9 +60,9 @@ namespace BrainCloud {
 		 * @param in_whereJson
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void getCount(const char * in_entityType, const std::string& in_whereJson, IServerCallback * in_callback = NULL);
+		void getCount(const char *in_entityType, const std::string &in_whereJson, IServerCallback *in_callback = NULL);
 
-		 /**
+		/**
 		 * Method uses a paging system to iterate through Custom Entities
 		 * After retrieving a page of Custom Entities with this method,
 		 * use GetEntityPageOffset() to retrieve previous or next pages.
@@ -75,7 +75,7 @@ namespace BrainCloud {
 		 *                   See the portal appendix documentation for format.
 		 * @param in_callback The callback object
 		 */
-		void getEntityPage(const char * in_entityType, const char * in_context, IServerCallback * in_callback = NULL);
+		void getEntityPage(const char *in_entityType, const char *in_context, IServerCallback *in_callback = NULL);
 
 		/**
 		 *
@@ -87,8 +87,7 @@ namespace BrainCloud {
 		 * @param in_maxReturn
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void getRandomEntitiesMatching(const char * in_entityType, const std::string& in_whereJson, const int32_t in_maxReturn, IServerCallback * in_callback = NULL);
-
+		void getRandomEntitiesMatching(const char *in_entityType, const std::string &in_whereJson, const int32_t in_maxReturn, IServerCallback *in_callback = NULL);
 
 		/**
 		 * Gets the page of custom entities from the server based on the encoded context and specified page offset.
@@ -101,19 +100,19 @@ namespace BrainCloud {
 		 * @param in_pageOffset
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void getEntityPageOffset(const char * in_entityType, const std::string& in_context, int32_t in_pageOffset, IServerCallback * in_callback = NULL);
+		void getEntityPageOffset(const char *in_entityType, const std::string &in_context, int32_t in_pageOffset, IServerCallback *in_callback = NULL);
 
 		/**
-		 * Gets the page of custom entities from the server based on the encoded context and specified page offset.
+		 * Reads the specified custom entity from the server.
 		 *
 		 * Service Name - CustomEntity
 		 * Service Operation - ReadEntity
 		 *
 		 * @param in_entityType The entity type as defined by the user
-		 * @param in_entityId
+		 * @param in_entityId The entity id as defined by the system
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void readEntity(const char * in_entityType, const char * in_entityId, IServerCallback * in_callback = NULL);
+		void readEntity(const char *in_entityType, const char *in_entityId, IServerCallback *in_callback = NULL);
 
 		/**
 		 * Replaces the specified custom entity's data, and optionally updates the acl and expiry, on the server.
@@ -130,7 +129,7 @@ namespace BrainCloud {
 		 * @param timeToLive
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void updateEntity(const char * in_entityType, const char * in_entityId, int32_t in_version, const std::string& in_jsonEntityData, const std::string& in_jsonEntityAcl, int64_t in_timeToLive, IServerCallback * in_callback = NULL);
+		void updateEntity(const char *in_entityType, const char *in_entityId, int32_t in_version, const std::string &in_jsonEntityData, const std::string &in_jsonEntityAcl, int64_t in_timeToLive, IServerCallback *in_callback = NULL);
 
 		/**
 		 * Replaces the specified custom entity's data, and optionally updates the acl and expiry, on the server.
@@ -144,22 +143,22 @@ namespace BrainCloud {
 		 * @param in_fieldsJson
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void updateEntityFields(const char * in_entityType, const char * in_entityId, int32_t in_version, const std::string& in_fieldsJson, IServerCallback * in_callback = NULL);
+		void updateEntityFields(const char *in_entityType, const char *in_entityId, int32_t in_version, const std::string &in_fieldsJson, IServerCallback *in_callback = NULL);
 
-        /**
-         * For sharded custom collection entities. Sets the specified fields within custom entity data on the server, enforcing ownership/ACL permissions.
-         *
-         * Service Name - CustomEntity
-         * Service Operation - UpdateEntityFieldsSharded
-         *
-         * @param in_entityType The entity type as defined by the user
-         * @param in_entityId
-         * @param in_version
-         * @param in_fieldsJson
-         * @param in_shardKeyJson The shard key field(s) and value(s), as JSON, applicable to the entity being updated.
-         * @param in_callback The method to be invoked when the server response is received
-         */
-        void updateEntityFieldsSharded(const char * in_entityType, const char * in_entityId, int32_t in_version, const std::string& in_fieldsJson, const std::string& in_shardKeyJson, IServerCallback * in_callback = NULL);
+		/**
+		 * For sharded custom collection entities. Sets the specified fields within custom entity data on the server, enforcing ownership/ACL permissions.
+		 *
+		 * Service Name - CustomEntity
+		 * Service Operation - UpdateEntityFieldsSharded
+		 *
+		 * @param in_entityType The entity type as defined by the user
+		 * @param in_entityId
+		 * @param in_version
+		 * @param in_fieldsJson
+		 * @param in_shardKeyJson The shard key field(s) and value(s), as JSON, applicable to the entity being updated.
+		 * @param in_callback The method to be invoked when the server response is received
+		 */
+		void updateEntityFieldsSharded(const char *in_entityType, const char *in_entityId, int32_t in_version, const std::string &in_fieldsJson, const std::string &in_shardKeyJson, IServerCallback *in_callback = NULL);
 
 		/**
 		 * deletes entities based on the delete criteria.
@@ -171,10 +170,10 @@ namespace BrainCloud {
 		 * @param in_deleteCriteria Json string of criteria wanted for deletion
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void deleteEntities(const char * in_entityType, const char * in_deleteCriteria, IServerCallback * in_callback = NULL);
+		void deleteEntities(const char *in_entityType, const char *in_deleteCriteria, IServerCallback *in_callback = NULL);
 
 		/**
-		 * Deletes the specified custom entity singleton, owned by the session's user, 
+		 * Deletes the specified custom entity singleton, owned by the session's user,
 		 * for the specified entity type, on the server.
 		 *
 		 * Service Name - CustomEntity
@@ -183,11 +182,11 @@ namespace BrainCloud {
 		 * @param in_entityType The entity type as defined by the user
 		 * @param in_version
 		 */
-		void deleteSingleton(const char * in_entityType, int32_t in_version, IServerCallback * in_callback = NULL);
+		void deleteSingleton(const char *in_entityType, int32_t in_version, IServerCallback *in_callback = NULL);
 
 		/**
-		 * Updates the singleton owned by the user for the specified custom entity type on the server, 
-		 * creating the singleton if it does not exist. 
+		 * Updates the singleton owned by the user for the specified custom entity type on the server,
+		 * creating the singleton if it does not exist.
 		 * This operation results in the owned singleton's data being completely replaced by the passed in JSON object.
 		 *
 		 * Service Name - CustomEntity
@@ -195,15 +194,15 @@ namespace BrainCloud {
 		 *
 		 * @param in_entityType The entity type as defined by the user
 		 * @param in_version
-		 * @param in_dataJson 
-		 * @param in_acl 
-		 * @param in_timeToLive 
+		 * @param in_dataJson
+		 * @param in_acl
+		 * @param in_timeToLive
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void updateSingleton(const char * in_entityType, int32_t in_version, const std::string& in_dataJson, const std::string& in_acl, int64_t in_timeToLive, IServerCallback * in_callback = NULL);
+		void updateSingleton(const char *in_entityType, int32_t in_version, const std::string &in_dataJson, const std::string &in_acl, int64_t in_timeToLive, IServerCallback *in_callback = NULL);
 
 		/**
-		 * Partially updates the data, of the singleton owned by the user for the specified custom entity type, 
+		 * Partially updates the data, of the singleton owned by the user for the specified custom entity type,
 		 * with the specified fields, on the server
 		 *
 		 * Service Name - CustomEntity
@@ -214,7 +213,7 @@ namespace BrainCloud {
 		 * @param in_fieldsJson
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void updateSingletonFields(const char * in_entityType, int32_t in_version, const std::string& in_fieldsJson, IServerCallback * in_callback = NULL);
+		void updateSingletonFields(const char *in_entityType, int32_t in_version, const std::string &in_fieldsJson, IServerCallback *in_callback = NULL);
 
 		/**
 		 * Reads the custom entity singleton owned by the session's user.
@@ -225,24 +224,35 @@ namespace BrainCloud {
 		 * @param in_entityType The entity type as defined by the user
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void readSingleton(const char * in_entityType, IServerCallback * in_callback = NULL);
+		void readSingleton(const char *in_entityType, IServerCallback *in_callback = NULL);
 
 		/**
-		 * Reads the custom entity singleton owned by the session's user.
+		 * Increments fields on the specified custom entity owned by the user on the server.
 		 *
 		 * Service Name - CustomEntity
 		 * Service Operation - IncrementData
 		 *
 		 * @param in_entityType The entity type as defined by the user
-		 * @param in_entityId
-		 * @param in_fieldsJson
+		 * @param in_entityId The entity id as defined by the system
+		 * @param in_fieldsJson Specific fields, as JSON, within entity's custom data, with respective increment amount.
 		 * @param in_callback The method to be invoked when the server response is received
 		 */
-		void incrementData(const char * in_entityType, const char * in_entityId, const std::string& in_fieldsJson, IServerCallback * in_callback = NULL);
+		void incrementData(const char *in_entityType, const char *in_entityId, const std::string &in_fieldsJson, IServerCallback *in_callback = NULL);
 
+		/**
+		 * Increments the specified fields, of the singleton owned by the user, by the specified amount within the custom entity data on the server.
+		 *
+		 * Service Name - CustomEntity
+		 * Service Operation - IncrementSingletonData
+		 *
+		 * @param in_entityType The type of custom entity being updated.
+		 * @param in_fieldsJson Specific fields, as JSON, within entity's custom data, with respective increment amount.
+		 * @param in_callback The method to be invoked when the server response is received
+		 */
+		void incrementSingletonData(const char *in_entityType, const std::string &in_fieldsJson, IServerCallback *in_callback = NULL);
 
 	private:
-		BrainCloudClient * m_client;
+		BrainCloudClient *m_client;
 	};
 }
 #if defined(__clang__)
